35b795b946f377627c57484edd48709fc4e0e10a
Park yoon <parkyoon97@gmail.com>
2023-07-18 01:36:03 +0900

    초기세팅

---
+++ wydtoday/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+  <div className='w-full h-60 flex m-0 10' >
+    <nav className='w-full h-60 flex m-0 10'>
+        <div>Icon</div>
+        <div>Category</div>
+        <div>Login</div>
+    </nav>
+  </div>);
+}
+
+export default Header;
